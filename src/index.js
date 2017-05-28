0d016d9da116e679921a4231e588c385438db2b9
Amanshu Kataria <amanshukataria@hotmail.com>
2017-05-28 12:47:47 +0530

    Added divider and MuiThemeProvider
    Added divider to sidebar for dividing the list.
    Added MuiThemeProvider for divider as well as other material-ui
    components to work.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import FlatButton from 'material-ui/FlatButton';
 import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import Divider from 'material-ui/Divider';
 
 function NavBar(){
   return(
@@ -14,9 +16,13 @@ function NavBar(){
 }
 
 const style={
-  borderBottom: '1px solid gray',
+
   textAlign: 'left',
   paddingLeft: '20px',
+};
+
+const dividerStyle={
+  marginLeft: "0px"
 };
 
 class SideBar extends React.Component{
@@ -26,11 +32,16 @@ class SideBar extends React.Component{
   render(){
     return(
       <ul className="sideMenu">
-      <li><FlatButton label="All Contacts" primary={true} fullWidth={true} labelStyle={{textTransform: 'capitalize'}} style={style} /></li>
-      <li><FlatButton label="Frequently Contacted" primary={true} fullWidth={true} labelStyle={{textTransform: 'capitalize'}} style={style} /></li>
-      <li><FlatButton label="Groups" primary={true} fullWidth={true} labelStyle={{textTransform: 'capitalize'}} style={style} /></li>
-      <li><FlatButton label="Settings" primary={true} fullWidth={true} labelStyle={{textTransform: 'capitalize'}} style={style} /></li>
+        <li><FlatButton label="All Contacts" primary={true} fullWidth={true} labelStyle={{textTransform: 'capitalize'}} style={style} /></li>
+        <Divider inset={true} style={dividerStyle} />
+        <li><FlatButton label="Frequently Contacted" primary={true} fullWidth={true} labelStyle={{textTransform: 'capitalize'}} style={style} /></li>
+        <Divider inset={true} style={dividerStyle} />
+        <li><FlatButton label="Groups" primary={true} fullWidth={true} labelStyle={{textTransform: 'capitalize'}} style={style} /></li>
+        <Divider inset={true} style={dividerStyle} />
+        <li><FlatButton label="Settings" primary={true} fullWidth={true} labelStyle={{textTransform: 'capitalize'}} style={style} /></li>
+        <Divider inset={true} style={dividerStyle} />
         <li><FlatButton label="Help" primary={true} fullWidth={true} labelStyle={{textTransform: 'capitalize'}} style={style} /></li>
+        <Divider inset={true} style={dividerStyle} />
       </ul>
     );
   }
@@ -62,10 +73,7 @@ class AddContact extends React.Component{
 class Main extends React.Component{
   render(){
     return(
-      <div>
-        <div className="navBar">
-          <NavBar />
-        </div>
+      <MuiThemeProvider>
         <div>
           <div className="navBar">
             <NavBar />
@@ -82,6 +90,7 @@ class Main extends React.Component{
             <AddContact />
           </div>
         </div>
+      </MuiThemeProvider>
     );
   }
 }
